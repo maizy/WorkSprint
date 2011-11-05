fc898d5d0456335d7bdfc1ccc756afa1709bfc94
Nikita Kovaliov <nikita@maizy.ru>
2011-11-05 15:10:49 +0300

    Simplify start dev server

--- run-devserver.sh
+++ run-devserver.sh
@@ -1,8 +1,16 @@
 #/bin/bash
 
+SCRIPT_PATH=$(cd ${0%/*} && echo $PWD/${0##*/})
+PROJECT_ROOT=`dirname "$SCRIPT_PATH"`
+WEB_ROOT="${PROJECT_ROOT}/work"
+
 DEV_HOST=$1
 if [ -z "${DEV_HOST}" ]; then
-    DEV_HOST='localhost:8100'
+    if [ -f "${PROJECT_ROOT}/devserver-default-host" ]; then
+        DEV_HOST=`cat "${PROJECT_ROOT}/devserver-default-host"`
+    else
+        DEV_HOST='localhost:8800'
+    fi
 fi
 
 SCRIPT_PATH=$(cd ${0%/*} && echo $PWD/${0##*/})
